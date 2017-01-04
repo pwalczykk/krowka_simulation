48974b5089515adf3358dd243467cac990f11959
Przemek <pwalczykk@gmail.com>
2017-01-04 01:23:52 +0100

    script for installing dependencies added

---
+++ install_deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get install ros-kinetic-gazebo-ros-control
+sudo apt-get install ros-kinetic-controller-manager
+sudo apt-get install ros-kinetic-ros-controllers
+sudo apt-get install ros-kinetic-ros-control
+sudo apt-get install ros-kinetic-opencv3
+sudo apt-get install ros-kinetic-cv-bridge
+sudo apt-get install ros-kinetic-image-transport
+sudo apt-get install ros-kinetic-dynamic-reconfigure
+
+
+echo "finished :)"
